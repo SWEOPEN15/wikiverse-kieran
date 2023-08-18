0cfab03355ddc5adedddb5a07484f7860d3d63f5
Kieran Barker <29986418+kieranbarker@users.noreply.github.com>
2023-08-18 12:25:10 +0100

    Add single page view

--- public/react/components/App.js
+++ public/react/components/App.js
@@ -1,32 +1,63 @@
-import React, { useState, useEffect } from 'react';
-import { PagesList } from './PagesList';
+import React, { useState, useEffect } from "react";
+import { PagesList } from "./PagesList";
 
 // import and prepend the api url to any fetch calls
-import apiURL from '../api';
+import apiURL from "../api";
 
 export const App = () => {
-
 	const [pages, setPages] = useState([]);
+	const [article, setArticle] = useState(null);
 
-	async function fetchPages(){
+	async function fetchArticle(slug) {
 		try {
-			const response = await fetch(`${apiURL}/wiki`);
-			const pagesData = await response.json();
-			setPages(pagesData);
+			const response = await fetch(`${apiURL}/wiki/${slug}`);
+			const articleData = await response.json();
+			setArticle(articleData);
 		} catch (err) {
-			console.log("Oh no an error! ", err)
+			console.log("Oh no an error! ", err);
 		}
 	}
 
 	useEffect(() => {
+		async function fetchPages() {
+			try {
+				const response = await fetch(`${apiURL}/wiki`);
+				const pagesData = await response.json();
+				setPages(pagesData);
+			} catch (err) {
+				console.log("Oh no an error! ", err);
+			}
+		}
+
 		fetchPages();
 	}, []);
 
-	return (
-		<main>	
-      <h1>WikiVerse</h1>
-			<h2>An interesting 📚</h2>
-			<PagesList pages={pages} />
-		</main>
-	)
-}
+	if (article) {
+		return (
+			<main>
+				<a
+					href="#"
+					onClick={function (event) {
+						event.preventDefault();
+						setArticle(null);
+					}}
+				>
+					Back to Wiki List
+				</a>
+				<h1>{article.title}</h1>
+				<ul>
+					<li>Author: {article.author.name}</li>
+					<li>Published: {article.createdAt}</li>
+				</ul>
+			</main>
+		);
+	} else {
+		return (
+			<main>
+				<h1>WikiVerse</h1>
+				<h2>An interesting 📚</h2>
+				<PagesList pages={pages} fetchArticle={fetchArticle} />
+			</main>
+		);
+	}
+};
--- public/react/components/Page.js
+++ public/react/components/Page.js
@@ -1,9 +1,18 @@
-import React from 'react';
+import React from "react";
 
-export const Page = (props) => {
+export const Page = props => {
+	function handleClick(event) {
+		event.preventDefault();
+		props.fetchArticle(props.page.slug);
+	}
 
-  return <>
-    <h3>{props.page.title}</h3>
-  </>
-} 
-	
+	return (
+		<>
+			<h3>
+				<a href="#" onClick={handleClick}>
+					{props.page.title}
+				</a>
+			</h3>
+		</>
+	);
+};
